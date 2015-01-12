cbd11aa49d590b02a2b8a8077f28f03d97aefe1b
Daniel Feichtinger <df@hyperledger.org>
2015-01-12 11:24:57 +0800

    Add create fn for issues.

---
+++ test/web/models/issue_test.exs
@@ -0,0 +1,38 @@
+defmodule Hyperledger.ModelTest.Issue do
+  use HyperledgerTest.Case
+  use Ecto.Model
+  
+  alias Hyperledger.Repo
+  alias Hyperledger.Issue
+  alias Hyperledger.Account
+  alias Hyperledger.Ledger
+  
+  setup do
+    Ledger.create(
+      hash: "abc",
+      public_key: "123",
+      primary_account_public_key: "cde")
+    :ok
+  end
+  
+  test "`create` inserts the record into the db" do
+    uuid = UUID.uuid4
+    Issue.create(
+      uuid: uuid,
+      ledger_hash: "abc",
+      amount: 100)
+
+    assert Repo.get(Issue, UUID.info(uuid)[:binary]) != nil
+  end
+
+  test "`create` also modifies the balance of the primary wallet" do
+    Issue.create(
+      uuid: UUID.uuid4,
+      ledger_hash: "abc",
+      amount: 100)
+
+    l = Repo.get(Ledger, "abc")
+    [a] = Repo.all assoc(l, :primary_account)
+    assert a.balance == 100
+  end
+end
--- web/models/issue.ex
+++ web/models/issue.ex
@@ -1,12 +1,33 @@
 defmodule Hyperledger.Issue do
   use Ecto.Model
 
+  alias Hyperledger.Repo
+  alias Hyperledger.Issue
+  alias Hyperledger.Ledger
+  alias Hyperledger.Account
+  
   schema "issues", primary_key: {:uuid, :uuid, []} do
     field :amount, :integer
     field :created_at, :datetime, default: Ecto.DateTime.local
     field :updated_at, :datetime, default: Ecto.DateTime.local
     
-    belongs_to :ledger, Hyperledger.Ledger,
+    belongs_to :ledger, Ledger,
       foreign_key: :ledger_hash, type: :string
   end
+  
+  def create(attrs) do
+    Repo.transaction fn ->
+      ledger = Repo.get(Ledger, attrs[:ledger_hash])
+      [account] = Repo.all assoc(ledger, :primary_account)
+
+      account = %{account | balance: (account.balance + attrs[:amount])}
+      |> Repo.update
+      %Issue{
+        uuid: UUID.info(attrs[:uuid])[:binary],
+        ledger_hash: attrs[:ledger_hash],
+        amount: attrs[:amount]}
+      |> Repo.insert
+    end
+  end
+  
 end
