243f91aa5545bf712e9ceaf095fda0c6061f6a22
Daniel Feichtinger <df@hyperledger.org>
2015-01-13 18:25:22 +0800

    Create fn for transfers.

---
+++ test/web/models/transfer_test.exs
@@ -0,0 +1,48 @@
+defmodule Hyperledger.ModelTest.Transfer do
+  use HyperledgerTest.Case
+  
+  alias Hyperledger.Repo
+  alias Hyperledger.Account
+  alias Hyperledger.Ledger
+  alias Hyperledger.Transfer
+  
+  setup do
+    Ledger.create(
+      hash: "abc",
+      public_key: "123",
+      primary_account_public_key: "cde")
+      
+    %Account{public_key: "234", ledger_hash: "abc", balance: 100}
+    |> Repo.insert
+    %Account{public_key: "345", ledger_hash: "abc"}
+    |> Repo.insert
+    
+    :ok
+  end
+  
+  test "`create` inserts the record into the db" do
+    uuid = UUID.uuid4
+    Transfer.create(
+      uuid: uuid,
+      source_public_key: "234",
+      destination_public_key: "345",
+      amount: 100)
+
+    assert Repo.get(Transfer, UUID.info(uuid)[:binary]) != nil
+  end
+
+  test "`create` modifies the balance of the source and dest wallet" do
+    uuid = UUID.uuid4
+    Transfer.create(
+      uuid: UUID.uuid4,
+      source_public_key: "234",
+      destination_public_key: "345",
+      amount: 100)
+
+    s = Repo.get(Account, "234")
+    assert s.balance == 0
+
+    d = Repo.get(Account, "345")
+    assert d.balance == 100
+  end
+end
--- web/models/transfer.ex
+++ web/models/transfer.ex
@@ -1,14 +1,43 @@
 defmodule Hyperledger.Transfer do
   use Ecto.Model
 
+  alias Hyperledger.Repo
+  alias Hyperledger.Transfer
+  alias Hyperledger.Account
+  
   schema "transfers", primary_key: {:uuid, :uuid, []} do
     field :amount, :integer
     field :created_at, :datetime, default: Ecto.DateTime.local
     field :updated_at, :datetime, default: Ecto.DateTime.local
     
-    belongs_to :source, Hyperledger.Account,
-      foreign_key: :source_public_key, type: :string
-    belongs_to :destination, Hyperledger.Account,
-      foreign_key: :destination_public_key, type: :string
+    belongs_to :source, Account,
+      foreign_key: :source_public_key,
+      references: :public_key,
+      type: :string
+    belongs_to :destination, Account,
+      foreign_key: :destination_public_key,
+      references: :public_key,
+      type: :string
   end
+  
+  def create(attrs) do
+    Repo.transaction fn ->
+      transfer = %Transfer{
+        uuid: UUID.info(attrs[:uuid])[:binary],
+        amount: attrs[:amount],
+        source_public_key: attrs[:source_public_key],
+        destination_public_key: attrs[:destination_public_key]
+      }
+        
+      [source] = Repo.all assoc(transfer, :source)
+      [destination] = Repo.all assoc(transfer, :destination)
+      
+      %{ source | balance: (source.balance - transfer.amount)}
+      |> Repo.update
+      %{ destination | balance: (destination.balance + transfer.amount)}
+      |> Repo.update
+      Repo.insert(transfer)
+    end
+  end
+  
 end
