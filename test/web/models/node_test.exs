71c9d89786d66d4e0a5550aa894e5696801db392
Daniel Feichtinger <df@hyperledger.org>
2015-01-22 15:29:15 +0800

    Node module will return self id.

--- test/web/models/node_test.exs
+++ test/web/models/node_test.exs
@@ -1,4 +1,14 @@
 defmodule Hyperledger.ModelTest.Node do
   use HyperledgerTest.Case
   use Ecto.Model
+  
+  alias Hyperledger.Repo
+  alias Hyperledger.Node
+  
+  test "self_id returns the id for the current node" do
+    System.put_env("NODE_URL", "http://localhost")
+    node = %Node{url: "http://localhost", public_key: "abc"}
+           |> Repo.insert
+    assert Node.self_id == node.id
+  end
 end
--- web/models/node.ex
+++ web/models/node.ex
@@ -1,6 +1,10 @@
 defmodule Hyperledger.Node do
   use Ecto.Model
   
+  import Ecto.Query, only: [from: 2]
+  
+  alias Hyperledger.Repo
+  alias Hyperledger.Node
   alias Hyperledger.PrepareConfirmation
   
   schema "nodes" do
@@ -12,4 +16,8 @@ defmodule Hyperledger.Node do
     has_many :prepare_confirmations, PrepareConfirmation
   end
   
+  def self_id do
+    [node] = Repo.all(from n in Node, where: n.url == ^System.get_env["NODE_URL"], select: n)
+    node.id
+  end
 end
