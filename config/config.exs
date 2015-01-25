5a084d9166779166644d992efe0f6095bd1624f0
Daniel Feichtinger <df@hyperledger.org>
2015-01-25 17:25:57 +0800

    Use Ecto helper to get system database url.

--- config/config.exs
+++ config/config.exs
@@ -19,7 +19,7 @@ config :logger, :console,
 
 # Configuration for Ecto
 config :hyperledger, Hyperledger.Repo,
-  url: System.get_env("DATABASE_URL")
+  url: {:system, "DATABASE_URL"}
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
