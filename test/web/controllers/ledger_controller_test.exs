0eb4d3a7bc0ea9a2208f7c9b422ecc50b20e2b8a
Daniel Feichtinger <df@hyperledger.org>
2015-03-23 19:32:05 -0700

    Ledger controller test uses new call fn.

--- test/web/controllers/ledger_controller_test.exs
+++ test/web/controllers/ledger_controller_test.exs
@@ -1,30 +1,26 @@
 defmodule Hyperledger.LedgerControllerTest do
   use HyperledgerTest.Case
     
-  alias Hyperledger.Router
   alias Hyperledger.Repo
   alias Hyperledger.LogEntry
   alias Hyperledger.Ledger
   
   setup do
-    node = create_node(1)
-    System.put_env("NODE_URL", node.url)
+    create_primary
     :ok
   end
   
   test "list ledgers" do
-    conn = call(Router, :get, "/ledgers")
+    conn = call(:get, "/ledgers")
     assert conn.status == 200
   end
   
   test "create ledger through log entry" do
     body = %{ledger: %{hash: "123", publicKey: "abc", primaryAccountPublicKey: "def"}}
-    conn = call(Router, :post, "/ledgers", body,
-      headers: [{"content-type", "application/json"}])
+    conn = call(:post, "/ledgers", body, [{"content-type", "application/json"}])
     
     assert conn.status == 201
     assert Repo.all(Ledger)   |> Enum.count == 1
     assert Repo.all(LogEntry) |> Enum.count == 1
   end
-    
 end
