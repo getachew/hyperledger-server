952c14ec74132acb50267a377b3e3e671cc27b2a
Daniel Feichtinger <df@hyperledger.org>
2015-05-04 11:19:19 +0800

    Bump deps and add new model case file for tests

--- mix.exs
+++ mix.exs
@@ -32,12 +32,12 @@ defmodule Hyperledger.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.11.0"},
+    [{:phoenix, "~> 0.12"},
      {:phoenix_ecto, "~> 0.3"},
      {:cowboy, "~> 1.0"},
      {:postgrex, "~> 0.8"},
      {:phoenix_live_reload, "~> 0.3"},
-     {:dotenv, "~> 0.0.4"},
+     {:dotenv, "~> 1.0"},
      {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.1"},
      {:httpotion, "~> 2.0.0"},
      {:mock, "~> 0.1.0"}]
